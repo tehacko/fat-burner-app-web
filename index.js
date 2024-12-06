d990fea7669450a7c656f3564520386d545282af
Cornelius Lundi <hajek@cmgp.cz>
2024-12-06 14:52:29 +0100

    Connecting the Page to a PostgreSQL DB

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "fat_burner_db",
+  password: "123456",
+  port: 5432,
+});
 
 const app = express();
 const port = 3000;
 
+var users_db_data = [];
+async function loadAllDBData() {
+  try {
+    db.connect();
+    const result = await db.query('SELECT * FROM users');
+    db.end();
+    return result.rows;
+  } catch (err) {
+    console.error('Error fetching data:', err);
+    return [];
+  }
+}
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 // const URL_API = ...
+
 let heading;
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  users_db_data = await loadAllDBData();
+  console.log(users_db_data);
   res.render("index.ejs", {
     confirmRegistration: heading,
-    userData: users,
+    userData: users_db_data,
   });
 });
 
@@ -20,11 +45,11 @@ app.get("/loggedinpage", (req, res) => {
   // Get the selected user ID
   const userId = parseInt(req.query.userId); 
   // Find the user
-  const selectedUser = users.find(user => user.id === userId); 
+  const selectedUser = users_db_data.find(user => user.id === userId); 
   if (selectedUser) {
     res.render("loggedinpage.ejs", {
-    // Pass the userName as an attribute of the res
-      currentUserName: selectedUser.userName
+    // Pass the username as an attribute of the res
+      currentUserName: selectedUser.username
     });
   } else {
     res.status(404).send("User not found!");
@@ -40,22 +65,24 @@ app.post("/submit", (req, res) => {
   const { username, sex, height, birthdate } = req.body;
   // Add a new user to the array
   const newUser = {
-    id: users.length + 1, // Incremental ID
-    userName: username,
+    id: users_db_data.length + 1, // Incremental ID
+    username: username,
     sex: sex,
     height: height,
     birthdate: birthdate,
   };
-  users.push(newUser);
+  users_db_data.push(newUser);
   heading = `${username}, you have been
        successfully temporarily registered!`;
   res.redirect("/");
-  console.log(users[users.length-1]);
+  console.log(users_db_data[users_db_data.length-1]);
   // 3. create a DB for the purpose
   // 4. write the data to DB
   // 5. say "${username}, you have been
   //     successfully registered."
 });
+
+
 
 
 // PUT method here (replace user data)
@@ -70,37 +97,37 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-var users = [
-  {
-    id: 1,
-    userName:
-      "Jeshuillo",
-    sex: "male",
-    height: "250",
-    birthdate: "0014-12-25",
-  },
-  {
-    id: 2,
-    userName:
-      "Immaculillo",
-    sex: "female",
-    height: "249",
-    birthdate: "0000-12-08",
-  },
-  {
-    id: 3,
-    userName:
-      "Tomadillo",
-    sex: "male",
-    height: "193",
-    birthdate: "1913-02-13",
-  },
-  {
-    id: 4,
-    userName:
-      "Marumillo",
-    sex: "female",
-    height: "173",
-    birthdate: "2002-11-29",
-  }
-];
+// var users = [
+//   {
+//     id: 1,
+//     username:
+//       "Jeshuillo",
+//     sex: "male",
+//     height: "250",
+//     birthdate: "0014-12-25",
+//   },
+//   {
+//     id: 2,
+//     username:
+//       "Immaculillo",
+//     sex: "female",
+//     height: "249",
+//     birthdate: "0000-12-08",
+//   },
+//   {
+//     id: 3,
+//     username:
+//       "Tomadillo",
+//     sex: "male",
+//     height: "193",
+//     birthdate: "1913-02-13",
+//   },
+//   {
+//     id: 4,
+//     userame:
+//       "Marumillo",
+//     sex: "female",
+//     height: "173",
+//     birthdate: "2002-11-29",
+//   }
+// ];
